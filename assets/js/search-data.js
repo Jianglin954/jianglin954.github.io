8dff7ea54329866a56039aa62254b970b17df2fa
Jianglin954 <JianglinLu@outlook.com>
2026-07-25 22:03:27 +0000

    Deploying to gh-pages from @ Jianglin954/jianglin954.github.io@6821d162c77d77affa2cabc0bd70cd297df05e1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
+        },{id: "nav-experience",
+          title: "experience",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/experience/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
